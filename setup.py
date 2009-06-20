e3b60cf41e951c8271ae742b3cec4a01af549870
Amorilia <amorilia@users.sourceforge.net>
2009-06-20 20:01:43 +0100

    Added license text and readme.

--- setup.py
+++ setup.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python
 
+"""Setup script for bdist-nsi."""
+
+classifiers = """Development Status :: 2 - Pre-Alpha
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Natural Language :: English
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Distribution
+Topic :: Code Generators"""
+
 from distutils.core import setup
 import os
-setup(  name = 'bdist_nsi',
-    version = '0.0.2a',
-    description = 'create a windows installer exe-program',
-    author = 'j-cg',
-    author_email = 'j-cg@users.sourceforge.net',
-    url = 'http://bdist-nsi.sourceforge.net/',
-    license = 'Python Software Foundation License',
+
+long_description = open("README.rst").read()
+
+setup(
+    name = 'bdist-nsi',
+    version = '0.1.0',
+    description = 'Create an NSIS windows installer.',
+    author = 'j-cg, amorilia',
+    author_email = 'amorilia@users.sourceforge.net',
+    url = 'http://github.com/amorilia/bdist-nsi',
+    license = 'BSD',
     platforms = ["any"],
-    classifiers = [
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: PSF License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Distribution',
-        'Topic :: Code Generators'],
-    long_description = "\ncreate a windows installer exe-program with nsis",
-    data_files = [['distutils/command',['bdist_nsi.py']]],
+    classifiers = filter(None, classifiers.split("\n")),
+    long_description = long_description,
+    data_files = [['distutils/command',['bdist_nsi.py']]]
 )
