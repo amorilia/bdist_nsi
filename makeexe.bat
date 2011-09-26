d9d55ffbe1d734f6ef5ab4d73d75b7efc28cf47e
Amorilia <amorilia@users.sourceforge.net>
2011-09-26 13:02:35 +0100

    Include py3k in installer scripts.

--- makeexe.bat
+++ makeexe.bat
@@ -4,5 +4,5 @@ rem a script for making an nsis installer for the bdist_nsi package
 git clean -x -f -d
 python setup.py build
 python setup.py install
-python setup.py --command-packages bdist_nsi bdist_nsi --target-versions=2.5,2.6,2.7
+python setup.py --command-packages bdist_nsi bdist_nsi --target-versions=2.5,2.6,2.7,3.0,3.1,3.2
 python setup.py sdist --formats zip
--- makeexe.sh
+++ makeexe.sh
@@ -5,6 +5,6 @@
 git clean -x -f -d
 python setup.py build
 su -c 'python setup.py install'
-python setup.py --command-packages bdist_nsi bdist_nsi --target-versions=2.5,2.6,2.7
+python setup.py --command-packages bdist_nsi bdist_nsi --target-versions=2.5,2.6,2.7,3.0,3.1,3.2
 python setup.py sdist --formats zip,bztar
 
