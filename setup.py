ae76c2e07a08d7f7f016e75be8b66143cc97dc2c
Amorilia <amorilia@users.sourceforge.net>
2011-09-26 13:05:56 +0100

    Version bump.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     name = 'bdist_nsi',
     packages = ['bdist_nsi'],
     package_data = {'': ['*.ico', '*.bmp']}, # include ico and bmp files
-    version = '0.1.3',
+    version = '0.1.4',
     description = 'Create NSIS windows installers for Python modules.',
     # note: author of the original http://bdist-nsi.sourceforge.net/ package
     #       (which formed the original basis of bdist_nsi) is j-cg
